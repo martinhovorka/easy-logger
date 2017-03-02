e6e49f0ca6a33fc4264f6916957aaec8f32ca165
Martin Hovorka <martinhovorka@users.noreply.github.com>
2017-03-02 11:49:47 +0100

    fixed typo in my name :)

--- easylogger.c
+++ easylogger.c
@@ -1,6 +1,6 @@
 /*
  *   EasyLogger - Simple thread-safe logging library
- *   Copyright (C) 2016, 2017  Martim Hovorka
+ *   Copyright (C) 2016, 2017  Martin Hovorka
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
